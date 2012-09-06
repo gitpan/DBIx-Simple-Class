1fce5faeeb33ca8674e003a92a9bc6acf26c3a7c
Красимир Беров <berov@cpan.org>
2012-09-06 22:28:14 +0000

    Import of BEROV/DBIx-Simple-Class-0.98 from CPAN.
    gitpan-cpan-distribution: DBIx-Simple-Class
    gitpan-cpan-version:      0.98
    gitpan-cpan-path:         BEROV/DBIx-Simple-Class-0.98.tar.gz
    gitpan-cpan-author:       BEROV
    gitpan-cpan-maturity:     released

---
+++ lib/DBIx/Simple/Class/Schema.pm
@@ -0,0 +1,407 @@
+package DBIx::Simple::Class::Schema;
+use strict;
+use warnings;
+use 5.10.1;
+use Carp;
+use parent 'DBIx::Simple::Class';
+use Data::Dumper;
+
+*_get_obj_args = \&DBIx::Simple::Class::_get_obj_args;
+
+#struct to keep schemas while building
+my $schemas = {};
+
+#for accessing schema structures during tests
+sub _schemas {
+  $_[2] && ($schemas->{$_[1]} = $_[2]);
+  return $_[1] && exists $schemas->{$_[1]} ? $schemas->{$_[1]} : $schemas;
+}
+
+sub _get_table_info {
+  my ($class, $args) = _get_obj_args(@_);
+
+  $args->{namespace} || croak('Please pass "namespace" argument');
+
+  #get tables from the current database
+  #see https://metacpan.org/module/DBI#table_info
+  return $schemas->{$args->{namespace}}{tables} = $class->dbh->table_info(
+    undef, undef,
+    $args->{table} || '%',
+    $args->{type}  || "'TABLE','VIEW'"
+  )->fetchall_arrayref({});
+
+}
+
+sub _get_column_info {
+  my ($class, $tables) = @_;
+  foreach my $table (@$tables) {
+    $table->{column_info} =
+      $class->dbh->column_info(undef, undef, $table->{TABLE_NAME}, '%')
+      ->fetchall_arrayref({});
+  }
+  return $tables;
+}
+
+#generates COLUMNS and PRIMARY_KEY
+sub _generate_PRIMARY_KEY_COLUMNS_ALIASES_CHECKS {
+  my ($class, $tables) = @_;
+
+  foreach my $t (@$tables) {
+
+    $t->{PRIMARY_KEY} =
+      $class->dbh->primary_key_info(undef, undef, $t->{TABLE_NAME})
+      ->fetchall_arrayref({})->[0]->{COLUMN_NAME} || '';
+
+    $t->{COLUMNS}           = [];
+    $t->{ALIASES}           = {};
+    $t->{CHECKS}            = {};
+    $t->{QUOTE_IDENTIFIERS} = 0;
+    foreach my $col (sort { $a->{ORDINAL_POSITION} <=> $b->{ORDINAL_POSITION} }
+      @{$t->{column_info}})
+    {
+      push @{$t->{COLUMNS}}, $col->{COLUMN_NAME};
+
+      #generate ALIASES
+      if ($col->{COLUMN_NAME} =~ /\W/) {    #not A-z0-9_
+        $t->{QUOTE_IDENTIFIERS} = 1;
+        $t->{ALIASES}{$col->{COLUMN_NAME}} = $col->{COLUMN_NAME};
+        $t->{ALIASES}{$col->{COLUMN_NAME}} =~ s/\W/_/g;    #foo-bar=>foo_bar
+      }
+      elsif ($class->SUPER::can($col->{COLUMN_NAME})) {
+        $t->{ALIASES}{$col->{COLUMN_NAME}} = 'column_' . $col->{COLUMN_NAME};
+      }
+
+      # generate CHECKS
+      if ($col->{IS_NULLABLE} eq 'NO') {
+        $t->{CHECKS}{$col->{COLUMN_NAME}}{required} = 1;
+        $t->{CHECKS}{$col->{COLUMN_NAME}}{defined}  = 1;
+      }
+      if ($col->{COLUMN_DEF}) {
+        my $default = $col->{COLUMN_DEF};
+        $default =~ s|\'||g;
+        $t->{CHECKS}{$col->{COLUMN_NAME}}{default} = $default;
+      }
+      my $size = $col->{COLUMN_SIZE} // 0;
+      if ($size >= 65535 || $size == 0) {
+        $size = '';
+      }
+      if ($col->{TYPE_NAME} =~ /INT/i) {
+        $t->{CHECKS}{$col->{COLUMN_NAME}}{allow} = qr/^-?\d{1,$size}$/x;
+      }
+      elsif ($col->{TYPE_NAME} =~ /FLOAT|DOUBLE|DECIMAL/i) {
+        my $scale     = $col->{DECIMAL_DIGITS};
+        my $precision = $size - $scale;
+        $t->{CHECKS}{$col->{COLUMN_NAME}}{allow} =
+          qr/^-?\d{1,$precision}(?:\.\d{1,$scale})?$/x;
+      }
+      elsif ($col->{TYPE_NAME} =~ /CHAR|TEXT|CLOB/i) {
+        $t->{CHECKS}{$col->{COLUMN_NAME}}{allow} = qr/^.{1,$size}$/x;
+      }
+    }    #end foreach @{$t->{column_info}
+  }    #end foreach $tables
+  return $tables;
+}
+
+
+sub _generate_CODE {
+  my ($class, $args) = @_;
+  my $code      = '';
+  my $namespace = $args->{namespace};
+  my $tables    = $schemas->{$namespace}{tables};
+  $schemas->{$namespace}{code} = [];
+
+  push @{$schemas->{$namespace}{code}}, <<"BASE_CLASS";
+package $namespace;
+use strict;
+use warnings;
+use parent 'DBIx::Simple::Class';
+our \$VERSION = '0.01';
+sub base_class{1}
+1;
+BASE_CLASS
+
+
+  foreach my $t (@$tables) {
+    my $package = $namespace . '::' . ucfirst(lc($t->{TABLE_NAME}));
+    my $COLUMNS = Data::Dumper->Dump([$t->{COLUMNS}], ['$COLUMNS']);
+    my $ALIASES = Data::Dumper->Dump([$t->{ALIASES}], ['$ALIASES']);
+    my $CHECKS  = Data::Dumper->Dump([$t->{CHECKS}], ['$CHECKS']);
+    my $TABLE   = Data::Dumper->Dump([$t->{TABLE_NAME}], ['$TABLE_NAME']);
+    push @{$schemas->{$namespace}{code}}, qq|package $package;
+| . qq|use strict;
+use warnings;
+use parent qw($namespace);
+| . qq|
+sub base_class{0}
+my $TABLE
+sub TABLE { \$TABLE_NAME }| . qq|
+sub PRIMARY_KEY{'$t->{PRIMARY_KEY}'}
+my $COLUMNS
+sub COLUMNS { \$COLUMNS }
+my $ALIASES
+sub ALIASES { \$ALIASES }
+my $CHECKS
+sub CHECKS { \$CHECKS }
+
+__PACKAGE__->QUOTE_IDENTIFIERS($t->{QUOTE_IDENTIFIERS});
+#__PACKAGE__->BUILD;#build accessors during load
+
+1;
+| . qq|$/__END__$/$/=pod$/$/=encoding utf8$/$/=head1 NAME
+
+$package - A class for $t->{TABLE_TYPE} $t->{TABLE_NAME} in schema $t->{TABLE_SCHEM}
+
+| . qq|=head1 SYNOPSIS$/$/=head1 DESCRIPTION$/$/=head1 COLUMNS$/
+Each column in this class has an accessor.
+| . qq|$/=head1 ALIASES$/$/=head1 GENERATOR$/$/L<$class>$/$/=head1 SEE ALSO
+$/$/|
+      . qq|L<$namespace>,L<DBIx::Simple::Class>, L<$class>|;
+  }    # end foreach my $t (@$tables)
+  if (defined wantarray) {
+    if (wantarray) {
+      return @{$schemas->{$namespace}{code}};
+    }
+    else {
+      return join '', @{$schemas->{$namespace}{code}};
+    }
+  }
+  return;
+}
+
+sub load_schema {
+  my ($class, $args) = _get_obj_args(@_);
+  unless ($args->{namespace}) {
+    $args->{namespace} = $class->dbh->{Name};
+    if ($args->{namespace} =~ /(database|dbname|db)=([^;]+);?/x) {
+      $args->{namespace} = $2;
+    }
+    $args->{namespace} =~ s/\W//xg;
+    $args->{namespace} = ucfirst(lc($args->{namespace}));
+  }
+
+  my $tables = $class->_get_table_info($args);
+
+  #get table columns
+  $class->_get_column_info($tables);
+
+  #generate COLUMNS, PRIMARY_KEY, ALIASES, CHECKS
+  $class->_generate_PRIMARY_KEY_COLUMNS_ALIASES_CHECKS($tables);
+
+  #generate code
+  if (defined wantarray) {
+    return $class->_generate_CODE($args);
+  }
+  else {
+    $class->_generate_CODE($args);
+  }
+  return;
+}
+
+
+sub dump_schema_at {
+  my ($class, $args) = _get_obj_args(@_);
+  $args->{lib_root} ||= $INC[0];
+  my ($namespace, @namespace, @base_path, $schema_path);
+
+  #_generate_CODE() should be called by now
+  $namespace = (keys %$schemas)[0];    #we always have only one key
+
+  $namespace || Carp::croak('Please first call ' . __PACKAGE__ . '->load_schema()!');
+
+  require Module::Load::Conditional;
+  require File::Path;
+  require File::Spec;
+  require IO::File;
+  @namespace = split /::/, $namespace;
+  @base_path = File::Spec->splitdir($args->{lib_root});
+
+  $schema_path = File::Spec->catdir(@base_path, @namespace);
+  if ((-f "$schema_path.pm" || -d $schema_path) && !$args->{overwrite}) {
+    carp($schema_path . ' exists. Quitting...');
+    return;
+  }
+  if (my $href = Module::Load::Conditional::check_install(module => $namespace)) {
+    carp( "$namespace found at $href->{file}.$/"
+        . "Please avoid namespace collisions. Quitting...");
+    return;
+  }
+  carp('Will dump schema at ' . $args->{lib_root});
+
+  #We we should be able to continue safely now...
+  my $tables = $schemas->{$namespace}{tables};
+  my $code   = $schemas->{$namespace}{code};
+  if (!$args->{overwrite} && !-d $schema_path) {
+    eval { File::Path::make_path($schema_path); }
+      || carp("Can not make path $schema_path.$/$!. Quitting...") && return;
+  }
+  my $base_fh = IO::File->new("> $schema_path.pm");
+  if (defined $base_fh) {
+    print $base_fh $code->[0];
+    $base_fh->close;
+  }
+  else {
+    carp("$schema_path.pm: $!. Quitting...");
+    return;
+  }
+
+  foreach my $i (0 .. @$tables - 1) {
+    my $filename = ucfirst(lc($tables->[$i]{TABLE_NAME})) . '.pm';
+    my $fh       = IO::File->new("> $schema_path/$filename");
+    if (defined $fh) {
+      print $fh $code->[$i + 1];
+      $fh->close;
+    }
+    else {
+      carp("$schema_path/$filename: $!. Quitting!");
+      return;
+    }
+  }
+  return 1;
+}
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+DBIx::Simple::Class::Schema - Create and use classes representing tables from a database
+
+=head1 SYNOPSIS
+
+  #Somewhere in a utility script or startup() fo your application.
+  DBIx::Simple::Class::Schema->dbix(DBIx::Simple->connect(...));
+  my $perl_code = DBIx::Simple::Class::Schema->load_schema(
+    namespace =>'My::Model',
+    table => '%',              #all tables from the current database
+    type  => "'TABLE','VIEW'", # make classes for tables and views
+  );
+
+  #Now eval() to use your classes.
+  eval $perl_code || croak($@);
+
+
+  #Or load and save it for more customisations and later usage.
+  DBIx::Simple::Class::Schema->load_schema(
+    namespace =>'My::Model',
+    table => '%',              #all tables from the current database
+    type  => "'TABLE','VIEW'", # make classes for tables and views
+  );
+  DBIx::Simple::Class::Schema->dump_schema_at(
+    lib_root => "$ENV{PERL_LOCAL_LIB_ROOT}/lib"
+    overwrite =>1 #overwrite existing files
+  ) || Carp::croak 'Something went wrong! See above...';
+
+
+=head1 DESCRIPTION
+
+DBIx::Simple::Class::Schema automates the creation of classes from
+database tables. You can use it when you want to prototype quickly
+your application. It is also very convenient as an initial generator and dumper of
+your classes representing your database tables.
+
+=head1 METHODS
+
+=head2 load_schema
+
+Class method.
+
+  Params:
+    namespace - String. The class name for your base class,
+      default: ucfirst(lc($databse))
+    table - SQL string for a LIKE clause,
+      default: '%'
+    type - SQL String for an IN clause.
+      default: "'TABLE','VIEW'"
+
+Extracts tables' information from the current connection and generates
+Perl classes representing those tabels or views.
+If called in list context returns an array with perl code for each package.
+The first package is the base class.
+If called in scalar context returns all the generated code as a string.
+In void context returns C<undefined>.
+The generated classes are saved internally and are available for use by
+L</dump_schema_at>.
+This makes it very convenient for quickly prototyping applications
+by just modifying tables in your database.
+
+  my $perl_code = DBIx::Simple::Class::Schema->load_schema();
+  eval $perl_code || croak($@);
+  @...
+  my $user = Dbname::User->find(2345);
+
+=head2 dump_schema_at
+
+Class method.
+
+  Params:
+    lib_root: String - Where classes will be dumped.
+      default: $INC[0]
+    overwrite: boolean -1/0 Should it overwrite existing classes with the same name?
+      default: 0
+
+Uses the generated code by L</load_schema> and saves each class on the disk.
+Does several checks:
+
+=over
+
+=item 1
+
+Checks if a file with the name of your base class exists and exits
+if the flag C<overwrite> is not set.
+
+=item 2
+
+Checks if there is a module with the same name as your base class installed
+and exits if there is such module. This is done to avoid namespace collisions.
+
+=item 3
+
+Checks if the files can be written to disk and exit immediately if there is a problem.
+
+=back
+
+For every check above issues the system warning so you, the developer, can decide what to do.
+Returns true on success.
+
+=head1 SUPPORTED DATABASE DRIVERS
+
+DBIx::Simple::Class::Schema strives to be DBD agnostic and
+uses only functionality specified by L<DBI>.
+This means that if a driver implements the methods specifyed in L<DBI> it is supported.
+However currently only tests for L<DBD::SQLite> and L<DBD::mysql> are written.
+Feel free to contribute with tests for your prefered driver.
+The following methods are used to retreive information form the database:
+
+=over
+
+=item * L<DBI/table_info>
+
+=item * L<DBI/column_info>
+
+=item * L<DBI/primary_key_info>
+
+=back
+
+=head1 SUPPORTED SQL TYPES
+
+Currently some minimal L<DBIx::Simple::Class/CHECKS> are automatically generated for TYPE_NAMEs
+matching C</INT/i>,C</FLOAT|DOUBLE|DECIMAL/i>, C</CHAR|TEXT|CLOB/i>.
+You are supposed to write your own business-specific checks.
+
+
+=head1 SEE ALSO
+
+L<DBIx::Simple::Class>, L<DBIx::Simple>, L<DBIx::Class::Schema::Loader>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Красимир Беров (Krasimir Berov).
+
+This program is free software, you can redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
+
+See http://dev.perl.org/licenses/ for more information.
+
---
+++ t/01-dbix-simple-class-schema-sqlite.t
@@ -0,0 +1,169 @@
+#!perl
+
+use 5.10.1;
+use strict;
+use warnings;
+use utf8;
+use Test::More;
+
+
+BEGIN {
+  eval { require DBD::SQLite; 1 }
+    or plan skip_all => 'DBD::SQLite required';
+  eval { DBD::SQLite->VERSION >= 1 }
+    or plan skip_all => 'DBD::SQLite >= 1.00 required';
+  use File::Basename 'dirname';
+  use Cwd;
+  use lib (Cwd::abs_path(dirname(__FILE__) . '/..') . '/examples/lib');
+}
+
+
+use DBI::Const::GetInfoType;
+use Data::Dumper;
+
+#Suppress some warnings from DBIx::Simple::Class during tests.
+local $SIG{__WARN__} = sub {
+  if (
+    $_[0] =~ /(Will\sdump\sschema\sat
+         |exists
+         |avoid\snamespace\scollisions
+         |\w+\.pm|make\spath)/x
+    )
+  {
+    my ($package, $filename, $line, $subroutine) = caller(2);
+    ok($_[0], $subroutine . " warns '$1' OK");
+  }
+  else {
+    warn $_[0];
+  }
+};
+
+use_ok('DBIx::Simple::Class::Schema');
+
+my $DSCS = 'DBIx::Simple::Class::Schema';
+my $dbix = DBIx::Simple->connect('dbi:SQLite:dbname=:memory:', {sqlite_unicode => 1});
+isa_ok(ref($DSCS->dbix($dbix)), 'DBIx::Simple');
+can_ok($DSCS, qw(load_schema dump_schema_at));
+
+#=pod
+#create some tables
+$dbix->query(<<'TAB');
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  group_id int(11) NOT NULL, -- COMMENT 'Primary group for this user'
+  login_name varchar(100) NOT NULL,
+  login_password varchar(100) NOT NULL, -- COMMENT 'Mojo::Util::md5_sum($login_name.$login_password)'
+  name varchar(255) NOT NULL DEFAULT '',
+  email varchar(255) NOT NULL DEFAULT 'email@domain.com',
+  disabled tinyint(1) NOT NULL DEFAULT '0',
+  balance DECIMAL(8,2) NOT NULL DEFAULT '0.00',
+  UNIQUE(login_name) ON CONFLICT ROLLBACK,
+  UNIQUE(email) ON CONFLICT ROLLBACK
+
+)
+TAB
+
+#=cut
+
+$dbix->query(<<'TAB');
+CREATE TABLE groups(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  group_name VARCHAR(12),
+  "is blocked" INT,
+  data TEXT
+  )
+TAB
+
+#BARE DEFAULTS
+like(
+  (eval { $DSCS->dump_schema_at() }, $@),
+  qr/Please first call/,
+  'dump_schema_at() croaks OK'
+);
+require File::Path;
+File::Path::remove_tree($INC[0] . '/Memory');
+unlink($INC[0] . '/Memory.pm');
+File::Path::remove_tree($INC[0] . '/Your');
+
+ok(my $code = $DSCS->load_schema(), 'scalar context OK');
+ok(my @code = $DSCS->load_schema(), 'list context OK');
+my $tables = $DSCS->_schemas('Memory')->{tables};
+ok((grep { $_->{TABLE_NAME} eq 'users' || $_->{TABLE_NAME} eq 'groups' } @$tables),
+  '_get_table_info works');
+my @column_infos = (
+  @{$tables->[0]->{column_info}},
+  @{$tables->[1]->{column_info}},
+  @{$tables->[2]->{column_info}}
+);
+is((grep { $_->{COLUMN_NAME} eq 'id' } @column_infos), 2, '_get_column_info works');
+my %alaiases =
+  (%{$tables->[0]->{ALIASES}}, %{$tables->[1]->{ALIASES}}, %{$tables->[2]->{ALIASES}});
+is((grep { $_ eq 'is_blocked' || $_ eq 'column_data' } values %alaiases),
+  2, '_generate_ALIASES works');
+
+my %checks =
+  (%{$tables->[0]->{CHECKS}}, %{$tables->[1]->{CHECKS}}, %{$tables->[2]->{CHECKS}});
+like('alaba_anica2', qr/$checks{group_name}->{allow}/, 'checks VARCHAR(12) works fine');
+unlike(
+  'alaba_anica13',
+  qr/$checks{group_name}->{allow}/,
+  'checks VARCHAR(12) works fine'
+);
+like('1',  qr/$checks{id}->{allow}/, 'checks INT works fine');
+like('11', qr/$checks{id}->{allow}/, 'checks INT works fine');
+unlike('a', qr/$checks{id}->{allow}/, 'checks INT works fine');
+like('1',          qr/$checks{data}->{allow}/, 'checks TEXT works fine');
+like('11sd,asd,a', qr/$checks{data}->{allow}/, 'checks TEXT works fine');
+unlike('', qr/$checks{'is blocked'}->{allow}/, 'checks TEXT works fine');
+like('1', qr/$checks{disabled}->{allow}/, 'checks TINYINT(1) works fine');
+unlike('11', qr/$checks{disabled}->{allow}/, 'checks TINYINT(1) works fine');
+unlike('a',  qr/$checks{disabled}->{allow}/, 'checks TINYINT(1) works fine');
+like('1',         qr/$checks{balance}->{allow}/, 'checks DECIMAL(8,2) works fine');
+like('11.2',      $checks{balance}->{allow},     'checks DECIMAL(8,2) works fine');
+like('123456.20', $checks{balance}->{allow},     'checks DECIMAL(8,2) works fine');
+unlike('1234567.2', $checks{balance}->{allow},     'checks DECIMAL(8,2) works fine');
+unlike('a',         qr/$checks{balance}->{allow}/, 'checks DECIMAL(8,2) works fine');
+ok((eval {$code}), 'code generated ok') or diag($@);
+ok($DSCS->dump_schema_at(), 'dump_schema_at dumps code to files OK');
+use_ok('Memory::Groups');
+use_ok('Memory::Users');
+
+#END BARE DEFAULTS
+#Now we should have some files to remove
+ok(!$DSCS->dump_schema_at(), 'quits OK');
+unlink($INC[0] . '/Memory.pm');
+ok(!$DSCS->dump_schema_at(), 'quits OK');
+ok($DSCS->dump_schema_at(overwrite => 1), 'overwrites OK');
+ok(!$DSCS->dump_schema_at(overwrite => 1), 'quits OK');
+unlink($INC[0] . '/Memory.pm');
+chmod 0444, $INC[0] . '/Memory/Users.pm';
+ok(!$DSCS->dump_schema_at(overwrite => 1), 'quits OK');
+chmod 0644, $INC[0] . '/Memory/Users.pm';
+File::Path::remove_tree($INC[0] . '/Memory');
+unlink($INC[0] . '/Memory.pm');
+
+#PARAMS
+delete $DSCS->_schemas->{Memory};
+$DSCS->load_schema(namespace => 'Your::Model', table => '%user%', type => "'TABLE'")
+  ;    #void context ok
+isa_ok($DSCS->_schemas('Your::Model'),
+  'HASH', 'load_schema creates Your::Model namespace OK');
+
+#warn Dumper($DSCS->_schemas('Your::Model')->{tables});
+is($DSCS->_schemas('Your::Model')->{tables}[0]->{TABLE_NAME},
+  'users', 'first table is "users"');
+is(scalar @{$DSCS->_schemas('Your::Model')->{tables}}, 1, 'the only table is "users"');
+chmod 0444, $INC[0];
+ok(!$DSCS->dump_schema_at(lib_root => $INC[0]), 'quits OK');
+chmod 0755, $INC[0];
+ok($DSCS->dump_schema_at(lib_root => $INC[0]), 'dumps OK');
+File::Path::remove_tree($INC[0] . '/Your');
+
+TODO: {
+  local $TODO = "load_schema, dump_schema_at - not finished";
+
+#load_schema
+}
+
+
+done_testing;
